b021d3fe3173b62066c7c7bb32bca81664b09662
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2023-01-12 09:45:01 +0100

    chore: replace app css with twind classes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
+import { tw } from 'twind'
+import { animation } from 'twind/css'
+
 import logo from './logo.svg';
-import './App.css';
+
+
+const logoSpin = animation('20s linear infinite', {
+  'from, 0%, to': {
+    transform: 'rotate(0deg)',
+  },
+  '100%': {
+    transform: 'rotate(360deg)',
+  },
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={tw`text-center`}>
+      <header className={tw`bg-[#282c34] min-h-screen flex flex-col items-center justify-center text-[calc(10px+2vmin)] text-white`}>
+        <img src={logo} className={tw`h-[40vmin] pointer-events-none motion-safe:animate-[${logoSpin}]`} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className={tw`text-[#61dafb]`}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
